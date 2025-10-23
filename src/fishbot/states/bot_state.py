1b9b3ba9dcb332470e22ebfc611fba9dd238105c
Franklin <franklinpereira98@hotmail.com>
2025-10-23 13:41:36 -0300

    Importing Interceptor Level Checker

--- src/fishbot/states/bot_state.py
+++ src/fishbot/states/bot_state.py
@@ -1,11 +1,14 @@
 from abc import ABC, abstractmethod
 
+
 class BotState(ABC):
+
     def __init__(self, bot):
         self.bot = bot
         self.config = bot.config
         self.detector = bot.detector
         self.controller = bot.controller
+        self.level_check_interceptor = bot.level_check_interceptor
 
     @abstractmethod
     def handle(self, screen):
