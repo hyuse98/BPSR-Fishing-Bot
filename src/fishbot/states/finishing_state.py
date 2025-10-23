eac36ee9b2b3e008555385e452aeffdb8dbdfed1
Franklin <franklinpereira98@hotmail.com>
2025-10-23 13:43:34 -0300

    Adding GuardRail 1: Level Checker on finishing_state.py

--- src/fishbot/states/finishing_state.py
+++ src/fishbot/states/finishing_state.py
@@ -9,9 +9,18 @@ class FinishingState(BotState):
         super().__init__(bot)
 
     def handle(self, screen):
+
+        # --- [GUARD RAIL 1 (Interceptor)] ---
+        new_state = self.level_check_interceptor.check(screen)
+        if new_state:
+            self.bot.log("[FINISHING] ⚠️ Level Check detectado durante o finalização.")
+            return new_state
+        # --- [FIM DO GUARD RAIL] ---
+
         pos = self.detector.find(screen, "continue")
 
         if pos:
+
             self.bot.log("[FINISHING] 🖱️  Clicando em 'Continue'...")
             self.controller.move_to(pos[0], pos[1])
             time.sleep(0.2)
