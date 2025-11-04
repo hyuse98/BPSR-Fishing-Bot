decbf0e27be1ddce2d4d7e0a04ddf36301bfbe7b
Franklin <franklinpereira98@hotmail.com>
2025-11-04 14:01:13 -0300

    Entrypoint logic

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
+import time
+
 from src.fishbot.core.fishing_bot import FishingBot
+from src.fishbot.core.game.hotkeys import Hotkeys
+from src.fishbot.utils.logger import log
+
+
+def main():
+    bot = FishingBot()
+    hotkeys = Hotkeys(bot)
+
+    bot.start()
+
+    log("[INFO] Pressione F1 para iniciar o bot.")
+
+    while not bot.is_stopped():
+        if not hotkeys.paused:
+            bot.update()
+
+        time.sleep(0.1)
+
+    log("[INFO] Bot Finalizado.")
+
 
 if __name__ == "__main__":
-    bot = FishingBot()
-    bot.run()
+    main()
